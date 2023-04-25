36f51fa2fef602065a1485d3d9118abbf337ce8b
Mahdi Ahmed <mahdiahmed@Mahdis-MacBook-Pro.local>
2023-04-26 02:40:39 +0600

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello again
+          Hello from AWS
         </p>
         <a
           className="App-link"
